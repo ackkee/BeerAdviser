31c3de766a769b87cc74bddb0a99315fd994ead1
Axel Lind <axel.lind@live.se>
2020-11-10 18:21:39 +0100

    Commit to 2nd pc

--- app/src/main/java/com/axel/myfirstapp/MainActivity.java
+++ app/src/main/java/com/axel/myfirstapp/MainActivity.java
@@ -10,14 +10,9 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    private Boolean hej = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b = findViewById(R.id.button);
-        b.setOnClickListener(e -> {
-
-        });
     }
 }
